6d43797cc230aa1b0f3457ac9a2813a9214657bf
yerasoni20 <yerasoni20@gmail.com>
2022-03-17 03:55:38 -0400

    Added Specialities Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/example/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package example.springframework.sfgpetclinic.services;
+
+import example.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/example/springframework/sfgpetclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,35 @@
+package example.springframework.sfgpetclinic.services.map;
+
+import example.springframework.sfgpetclinic.model.Speciality;
+import example.springframework.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Speciality,Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.finsAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality Object) {
+        return super.save(Object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
