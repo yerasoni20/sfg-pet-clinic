2cc0e1bf934c1b68ae33441396798d9164a31ece
yerasoni20 <yerasoni20@gmail.com>
2022-03-17 02:34:18 -0400

    added PetTypeService. closes #29.

---
+++ pet-clinic-data/src/main/java/example/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package example.springframework.sfgpetclinic.services;
+
+import example.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
