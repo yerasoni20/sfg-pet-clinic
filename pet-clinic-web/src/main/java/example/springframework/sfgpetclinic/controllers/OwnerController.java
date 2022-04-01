81de01636cd0007a380bc0a7df2db265bd63a3b3
yerasoni20 <yerasoni20@gmail.com>
2022-04-01 07:07:12 -0400

    Fix Display Owner. Closes #65.

--- pet-clinic-web/src/main/java/example/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/example/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,9 +1,13 @@
 package example.springframework.sfgpetclinic.controllers;
 
 import example.springframework.sfgpetclinic.services.OwnerService;
+import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping({"/owners"})
 @Controller
@@ -26,5 +30,13 @@ public class OwnerController {
     public String findOwners()
     {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId)
+    {
+        ModelAndView mav=new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
