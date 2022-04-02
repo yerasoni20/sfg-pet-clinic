8bef26e52181e93eb697f4791bd863b3c2f2293b
yerasoni20 <yerasoni20@gmail.com>
2022-04-02 12:53:45 -0400

    Updated PetMapServiceTest test class.

--- pet-clinic-data/src/test/java/example/springframework/sfgpetclinic/services/map/PetMapServiceTest.java
+++ pet-clinic-data/src/test/java/example/springframework/sfgpetclinic/services/map/PetMapServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Set;
 import javax.persistence.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PetMapServiceTest {
 
     private PetMapService petMapService;
